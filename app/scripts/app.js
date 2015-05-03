e59483cee9721cf8c4bae723184060aa31f1ade6
math29 <maaath29@gmail.com>
2015-05-03 09:47:52 -0700

    menu accordeon OP

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -12,25 +12,57 @@ var app = angular
   .module('firstAppAngularApp', []);
 
 app.controller("TitleController", function($scope){
-	$scope.title = "Titre";
-	$scope.text = "Contenu";
+	$scope.expanders = [
+		{title: 'Title 1',
+		text: 'Contenu 1'},
+		{title: 'Title 2',
+		text: 'Contenu 2'},
+		{title: 'Title 3',
+		text: 'Contenu 3'}
+	];
 });
 
-app.directive("expander", function(){
+app.directive("accordion", function(){
 	return {
 		restrict: 'EA',
 		replace: true,
 		transclude: true,
-		scope: {title:'=expanderTitle'},
-		template:'<div>'+
-		'<div class="title" ng-click="toogle()">{{title}}</div>'+
-		'<div class="body" ng-show="showMe" ng-transclude></div>'+
-		'</div>',
-		link: function(scope, element, attrs){
-			scope.showMe = false;
-			scope.toogle = function toogle(){
-				scope.showMe = !scope.showMe;
+		template:"<div ng-transclude></div>",
+		controller: function(){
+			var expanders = [];
+			this.gotOpened = function(selectedExpander){
+				expanders.forEach(function(expander){
+					if(selectedExpander != expander){
+						expander.showMe = false;
+					}
+				});
+			};
+			this.addExpander = function(expander){
+				expanders.push(expander);
 			};
 		}
 	};
+});
+
+
+app.directive("expander", function(){
+    return {
+        restrict: 'EA',
+        replace: true,
+        transclude: true,
+        require: '^?accordion',
+        scope: {title: '=expanderTitle'},
+        template: '<div>' +
+        '<div class="title" ng-click="toggle()">{{title}}</div>' +
+        '<div class="body" ng-show="showMe" ng-transclude></div>' +
+        '</div>',
+        link: function(scope, element, attrs, accordionController){
+            scope.showMe = false;
+            accordionController.addExpander(scope);
+            scope.toggle = function toggle(){
+                scope.showMe = !scope.showMe;
+                accordionController.gotOpened(scope);
+            };
+        }
+    };
 });
