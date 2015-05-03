4513134590a04c75b20bb46d4b20df73353e5cd6
math29 <maaath29@gmail.com>
2015-05-03 09:27:47 -0700

    fix bug grunt serve + change test expander

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,5 +8,29 @@
  *
  * Main module of the application.
  */
-angular
+var app = angular
   .module('firstAppAngularApp', []);
+
+app.controller("TitleController", function($scope){
+	$scope.title = "Titre";
+	$scope.text = "Contenu";
+});
+
+app.directive("expander", function(){
+	return {
+		restrict: 'EA',
+		replace: true,
+		transclude: true,
+		scope: {title:'=expanderTitle'},
+		template:'<div>'+
+		'<div class="title" ng-click="toogle()">{{title}}</div>'+
+		'<div class="body" ng-show="showMe" ng-transclude></div>'+
+		'</div>',
+		link: function(scope, element, attrs){
+			scope.showMe = false;
+			scope.toogle = function toogle(){
+				scope.showMe = !scope.showMe;
+			};
+		}
+	};
+});
